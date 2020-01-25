c4fd2df14c5d3a0aa274ecf444fa4bdef020dd8f
Tara Pun <pun.tara@outlook.com>
2020-01-24 17:18:24 -0800

    first commit

---
+++ deleteDuplicate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace deleteDuplicate
+{
+    class Program
+    {
+        public static int deleteDuplicate(int[] arr)
+        {
+            int Array = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != arr[Array])
+                {
+                    Array++;
+                    arr[Array] = arr[i];
+                }
+            }
+            return Array +1;
+        }
+        static void Main(string[] args)
+        {
+            int[] numArray = { 1, 1, 1, 2, 2, 2, 3, 4, 5, 5, 5, 5, 5, 6 };
+
+            int x = deleteDuplicate(numArray);
+            for(int i = 0; i < x; i++)
+            {
+                Console.Write(numArray[i] + " ");
+            }
+         
+        }
+    }
+}
